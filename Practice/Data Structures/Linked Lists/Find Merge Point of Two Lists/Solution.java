923d9dfdeabb10df1add855344e07c9a551a8e13
Ankit <codeaprendiz@gmail.com>
2018-09-10 09:33:48 +0530

    Create Solution.java

---
+++ Practice/Data Structures/Linked Lists/Find Merge Point of Two Lists/Solution.java
@@ -0,0 +1,187 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    static class SinglyLinkedListNode {
+        public int data;
+        public SinglyLinkedListNode next;
+
+        public SinglyLinkedListNode(int nodeData) {
+            this.data = nodeData;
+            this.next = null;
+        }
+    }
+
+    static class SinglyLinkedList {
+        public SinglyLinkedListNode head;
+        public SinglyLinkedListNode tail;
+
+        public SinglyLinkedList() {
+            this.head = null;
+            this.tail = null;
+        }
+
+        public void insertNode(int nodeData) {
+            SinglyLinkedListNode node = new SinglyLinkedListNode(nodeData);
+
+            if (this.head == null) {
+                this.head = node;
+            } else {
+                this.tail.next = node;
+            }
+
+            this.tail = node;
+        }
+    }
+
+    public static void printSinglyLinkedList(SinglyLinkedListNode node, String sep, BufferedWriter bufferedWriter) throws IOException {
+        while (node != null) {
+            bufferedWriter.write(String.valueOf(node.data));
+
+            node = node.next;
+
+            if (node != null) {
+                bufferedWriter.write(sep);
+            }
+        }
+    }
+        // Complete the findMergeNode function below.
+
+    /*
+     * For your reference:
+     *
+     * SinglyLinkedListNode {
+     *     int data;
+     *     SinglyLinkedListNode next;
+     * }
+     *
+     */
+    static int findMergeNode(SinglyLinkedListNode head1, SinglyLinkedListNode head2) {
+        return getNode(head1,head2);
+
+    }
+
+        /*function to get the intersection point of two linked 
+    lists head1 and head2 */
+    static int getNode(SinglyLinkedListNode head1, SinglyLinkedListNode head2) { 
+        int c1 = getCount(head1); 
+        int c2 = getCount(head2); 
+        int d; 
+  
+        if (c1 > c2) { 
+            d = c1 - c2; 
+            return _getIntesectionNode(d, head1, head2); 
+        } else { 
+            d = c2 - c1; 
+            return _getIntesectionNode(d, head2, head1); 
+        } 
+    } 
+  
+    /* function to get the intersection point of two linked 
+     lists head1 and head2 where head1 has d more nodes than 
+     head2 */
+    static int _getIntesectionNode(int d, SinglyLinkedListNode node1, SinglyLinkedListNode node2) { 
+        int i; 
+        SinglyLinkedListNode current1 = node1; 
+        SinglyLinkedListNode current2 = node2; 
+        for (i = 0; i < d; i++) { 
+            if (current1 == null) { 
+                return -1; 
+            } 
+            current1 = current1.next; 
+        } 
+        while (current1 != null && current2 != null) { 
+            if(current1.hashCode()==current2.hashCode()) {          
+                    return current1.data; 
+            } 
+            current1 = current1.next; 
+            current2 = current2.next; 
+        } 
+  
+        return -1; 
+    } 
+  
+    /*Takes head pointer of the linked list and 
+    returns the count of nodes in the list */
+    static int getCount(SinglyLinkedListNode node) { 
+        SinglyLinkedListNode current = node; 
+        int count = 0; 
+  
+        while (current != null) { 
+            count++; 
+            current = current.next; 
+        } 
+  
+        return count; 
+    }
+    
+        private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int tests = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int testsItr = 0; testsItr < tests; testsItr++) {
+            int index = scanner.nextInt();
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            SinglyLinkedList llist1 = new SinglyLinkedList();
+
+            int llist1Count = scanner.nextInt();
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            for (int i = 0; i < llist1Count; i++) {
+                int llist1Item = scanner.nextInt();
+                scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+                llist1.insertNode(llist1Item);
+            }
+          
+          	SinglyLinkedList llist2 = new SinglyLinkedList();
+
+            int llist2Count = scanner.nextInt();
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            for (int i = 0; i < llist2Count; i++) {
+                int llist2Item = scanner.nextInt();
+                scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+                llist2.insertNode(llist2Item);
+            }
+          
+          	SinglyLinkedListNode ptr1 = llist1.head;
+            SinglyLinkedListNode ptr2 = llist2.head;
+
+            for (int i = 0; i < llist1Count; i++) {
+                if (i < index) {
+                    ptr1 = ptr1.next;
+                }
+            }
+
+            for (int i = 0; i < llist2Count; i++) {
+                if (i != llist2Count-1) {
+                    ptr2 = ptr2.next;
+                }
+            }
+
+            ptr2.next = ptr1;
+
+            int result = findMergeNode(llist1.head, llist2.head);
+
+            bufferedWriter.write(String.valueOf(result));
+            bufferedWriter.newLine();
+        }
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
